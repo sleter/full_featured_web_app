f908ba8213c596f10d9b5cb4f0ab62690729542f
sleter <szy.janowski@gmail.com>
2018-05-06 12:46:57 +0200

    accounts

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from  flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 #needed for RegistrationForm and LoginForm -> protect against modyfing cookies, cross site request forgery attack
@@ -7,5 +9,11 @@ app.config['SECRET_KEY'] = '55d102a68a11bdb91578610ca74640a3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ddlotpgl:w4Pt-4b-ghnT3B77ln3czUoFcBwJyNfm@horton.elephantsql.com:5432/ddlotpgl'
 
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app.models import User, Post
 from app.forms import RegistrationForm, LoginForm
-from flask import render_template, url_for, flash, redirect
-from app import app
+from flask import render_template, url_for, flash, redirect, request
+from app import app, db, bcrypt
+from flask_login import login_user, current_user, logout_user, login_required
 
 #test_data
 posts = [
@@ -33,20 +34,43 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if(form.validate_on_submit()):
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         #warning it works only with python 3.6.5 (use .format for older versions)
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if(form.validate_on_submit()):
-        if(form.email.data == 'admin@wp.pl' and form.password.data == '12345'):
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if(user and bcrypt.check_password_hash(user.password, form.password.data)):
+            login_user(user, remember=form.remember.data)
+            #if we want to access page only for logged in users it will redirect after loggin
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('You fucked up logging in x-D', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file)
