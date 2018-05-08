e8c5f770221724eca93a51240b045cd8022ceaa5
Szymon Janowski <szy.janowski@gmail.com>
2018-05-08 15:01:57 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@
 class Config:
     # needed for RegistrationForm and LoginForm -> protect against modyfing cookies, cross site request forgery attack
     SECRET_KEY = '55d102a68a11bdb91578610ca74640a3'
-    SQLALCHEMY_DATABASE_URI = 'postgres://ddlotpgl:62z66ApgTiJXDhajQl7GhyAgaku933yo@horton.elephantsql.com:5432/ddlotpgl'
+    SQLALCHEMY_DATABASE_URI = ''
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
